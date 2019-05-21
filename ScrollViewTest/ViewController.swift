edbc065f7830fc5be38a1039a52188a21a19a24e
Oscar Manzelli <omanzelli@gmail.com>
2019-05-21 21:23:29 +0200

    first

--- ScrollViewTest/ViewController.swift
+++ ScrollViewTest/ViewController.swift
@@ -8,11 +8,29 @@
 
 import UIKit
 
+extension UIScrollView {
+    func updateContentView() {
+        contentSize.height = subviews.sorted(by: { $0.frame.maxY < $1.frame.maxY }).last?.frame.maxY ?? contentSize.height
+    }
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var contentView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        contentView.frame.size.height = 1000
+        
+        self.scrollView.updateContentView()
+        
+        print(self.contentView.frame.height)
+        print(self.scrollView.frame.height)
+        print(self.scrollView.contentSize.height)
+       
     }
 
 
